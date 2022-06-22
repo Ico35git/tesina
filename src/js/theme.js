3272151913d59519daf74a7ee18b97350a286b98
Ico35git <89799511+Ico35git@users.noreply.github.com>
2022-06-22 12:09:20 +0200

    Create theme.js

---
+++ src/js/theme.js
@@ -0,0 +1,7 @@
+import options from "../json/options.json";
+
+if(options.tema == "light") {
+    document.body.style.backgroundColor = "white";
+}else {
+    document.body.style.backgroundColor = "rgb(56, 58, 63)";
+}
